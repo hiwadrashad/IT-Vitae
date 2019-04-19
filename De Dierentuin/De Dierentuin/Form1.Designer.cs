286a2c1289d328f4fc1d759cbf361cf0f4c0addc
hiwadrashad <41074761+hiwadrashad@users.noreply.github.com>
2019-04-19 10:55:27 +0200

    Add files via upload

---
+++ De Dierentuin/De Dierentuin/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace De_Dierentuin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FeedAll = new System.Windows.Forms.Button();
+            this.FeedMonkeys = new System.Windows.Forms.Button();
+            this.FeedLions = new System.Windows.Forms.Button();
+            this.FeedElephants = new System.Windows.Forms.Button();
+            this.CreateMonkey = new System.Windows.Forms.Button();
+            this.CreateLion = new System.Windows.Forms.Button();
+            this.CreateElephant = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // FeedAll
+            // 
+            this.FeedAll.Location = new System.Drawing.Point(12, 12);
+            this.FeedAll.Name = "FeedAll";
+            this.FeedAll.Size = new System.Drawing.Size(175, 54);
+            this.FeedAll.TabIndex = 0;
+            this.FeedAll.Text = "Feed all animals";
+            this.FeedAll.UseVisualStyleBackColor = true;
+            this.FeedAll.Click += new System.EventHandler(this.FeedAll_Click);
+            // 
+            // FeedMonkeys
+            // 
+            this.FeedMonkeys.Location = new System.Drawing.Point(193, 12);
+            this.FeedMonkeys.Name = "FeedMonkeys";
+            this.FeedMonkeys.Size = new System.Drawing.Size(175, 54);
+            this.FeedMonkeys.TabIndex = 1;
+            this.FeedMonkeys.Text = "Feed the monkeys";
+            this.FeedMonkeys.UseVisualStyleBackColor = true;
+            this.FeedMonkeys.Click += new System.EventHandler(this.FeedMonkeys_Click);
+            // 
+            // FeedLions
+            // 
+            this.FeedLions.Location = new System.Drawing.Point(374, 12);
+            this.FeedLions.Name = "FeedLions";
+            this.FeedLions.Size = new System.Drawing.Size(175, 54);
+            this.FeedLions.TabIndex = 2;
+            this.FeedLions.Text = "Feed the lions";
+            this.FeedLions.UseVisualStyleBackColor = true;
+            this.FeedLions.Click += new System.EventHandler(this.FeedLions_Click);
+            // 
+            // FeedElephants
+            // 
+            this.FeedElephants.Location = new System.Drawing.Point(555, 12);
+            this.FeedElephants.Name = "FeedElephants";
+            this.FeedElephants.Size = new System.Drawing.Size(175, 54);
+            this.FeedElephants.TabIndex = 3;
+            this.FeedElephants.Text = "Feed the elephants";
+            this.FeedElephants.UseVisualStyleBackColor = true;
+            this.FeedElephants.Click += new System.EventHandler(this.FeedElephants_Click);
+            // 
+            // CreateMonkey
+            // 
+            this.CreateMonkey.Location = new System.Drawing.Point(93, 72);
+            this.CreateMonkey.Name = "CreateMonkey";
+            this.CreateMonkey.Size = new System.Drawing.Size(184, 68);
+            this.CreateMonkey.TabIndex = 4;
+            this.CreateMonkey.Text = "Create monkey";
+            this.CreateMonkey.UseVisualStyleBackColor = true;
+            this.CreateMonkey.Click += new System.EventHandler(this.CreateMonkey_Click);
+            // 
+            // CreateLion
+            // 
+            this.CreateLion.Location = new System.Drawing.Point(283, 72);
+            this.CreateLion.Name = "CreateLion";
+            this.CreateLion.Size = new System.Drawing.Size(184, 68);
+            this.CreateLion.TabIndex = 5;
+            this.CreateLion.Text = "Create lion";
+            this.CreateLion.UseVisualStyleBackColor = true;
+            this.CreateLion.Click += new System.EventHandler(this.CreateLion_Click);
+            // 
+            // CreateElephant
+            // 
+            this.CreateElephant.Location = new System.Drawing.Point(473, 72);
+            this.CreateElephant.Name = "CreateElephant";
+            this.CreateElephant.Size = new System.Drawing.Size(184, 68);
+            this.CreateElephant.TabIndex = 6;
+            this.CreateElephant.Text = "Create elephant";
+            this.CreateElephant.UseVisualStyleBackColor = true;
+            this.CreateElephant.Click += new System.EventHandler(this.CreateElephant_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(102, 188);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(175, 212);
+            this.listBox.TabIndex = 7;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(415, 188);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(242, 212);
+            this.dataGridView.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 412);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.CreateElephant);
+            this.Controls.Add(this.CreateLion);
+            this.Controls.Add(this.CreateMonkey);
+            this.Controls.Add(this.FeedElephants);
+            this.Controls.Add(this.FeedLions);
+            this.Controls.Add(this.FeedMonkeys);
+            this.Controls.Add(this.FeedAll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FeedAll;
+        private System.Windows.Forms.Button FeedMonkeys;
+        private System.Windows.Forms.Button FeedLions;
+        private System.Windows.Forms.Button FeedElephants;
+        private System.Windows.Forms.Button CreateMonkey;
+        private System.Windows.Forms.Button CreateLion;
+        private System.Windows.Forms.Button CreateElephant;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+    }
+}
+
---
+++ De Dierentuin/De Dierentuin/Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace De_Dierentuin
+{
+    public partial class Form1 : Form
+    {
+
+        static string name = "";
+        static int energy = 0;
+        static int eat = 25;
+        DataTable dtbl = new DataTable();
+        bool createTable = true;
+
+
+
+        abstract class UseEnergy
+        {
+            public abstract int monkeyEnergy { get; }
+            public abstract int lionEnergy { get; }
+            public abstract int elephantEnergy { get; }
+        }
+
+        class DerivedClass : UseEnergy
+        {
+            public override int monkeyEnergy
+            {
+                get
+                {
+                    return 2;
+                }
+            }
+            public override int lionEnergy
+            {
+                get
+                {
+                    return 10;
+                }
+            }
+            public override int elephantEnergy
+            {
+                get
+                {
+                    return 5;
+                }
+            }
+        }
+
+
+        class Animal
+        {
+            public static string Name()
+            {
+                return name;
+            }
+            public static int Energy()
+            {
+                return energy;
+            }
+            public static int Eat()
+            {
+                return eat;
+            }
+
+        }
+
+        class Monkey : Animal
+        {
+
+            public new static string Name()
+            {
+                return "Monkey";
+            }
+            public new static int Energy()
+            {
+                return 20;
+            }
+            public new static int Eat()
+            {
+                return eat * 10;
+            }
+        }
+
+        class Lion : Animal
+        {
+            public new static string Name()
+            {
+                return "Lion";
+            }
+            public new static int Energy()
+            {
+                return 10;
+            }
+            public new static int Eat()
+            {
+                return eat * 25;
+            }
+
+        }
+
+        class LionNonlead : Lion
+        {
+            public new static string Name()
+            {
+                return "Lion";
+            }
+            public new static int Energy()
+            {
+                return 10;
+            }
+            public new static int Eat()
+            {
+                return eat * 25;
+            }
+        }
+
+        class Elephant : Animal
+        {
+            public new static string Name()
+            {
+                return "Elephant";
+            }
+            public new static int Energy()
+            {
+                return 10;
+            }
+            public new static int Eat()
+            {
+                return eat * 50;
+            }
+        }
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int amountofmonkeys = 0;
+        static int amountoflions = 0;
+        static int amountofelephants = 0;
+
+        private void FeedAll_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < listBox.Items.Count)
+            {
+                string storage = Convert.ToString(listBox.Items[i]);
+                if (storage.Contains(Convert.ToString(Monkey.Name())))
+                {
+                    string animaltext = Convert.ToString(listBox.Items[i]);
+                    string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(Monkey.Name()) }, StringSplitOptions.None).Last();
+                    string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                    string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                    int energyinnumbers = Convert.ToInt32(animalenergy);
+                    string feededenergy = Convert.ToString(energyinnumbers + Monkey.Eat());
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(Monkey.Name() + animalnumber + " Energy:" + feededenergy));
+                    dtbl.Rows[i][0] = (Convert.ToString(Monkey.Name()) + animalnumber);
+                    dtbl.Rows[i][1] = feededenergy;
+                    i++;
+                }
+                else
+                if (storage.Contains(Convert.ToString(LionNonlead.Name())))
+                {
+                    string animaltext = Convert.ToString(listBox.Items[i]);
+                    string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(LionNonlead.Name()) }, StringSplitOptions.None).Last();
+                    string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                    string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                    int energyinnumbers = Convert.ToInt32(animalenergy);
+                    string feededenergy = Convert.ToString(energyinnumbers + LionNonlead.Eat());
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(LionNonlead.Name() + animalnumber + " Energy:" + feededenergy));
+                    dtbl.Rows[i][0] = (Convert.ToString(LionNonlead.Name()) + animalnumber);
+                    dtbl.Rows[i][1] = feededenergy;
+                    i++;
+                }
+                else
+                if (storage.Contains(Convert.ToString(Elephant.Name())))
+                {
+                    string animaltext = Convert.ToString(listBox.Items[i]);
+                    string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(Elephant.Name()) }, StringSplitOptions.None).Last();
+                    string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                    string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                    int energyinnumbers = Convert.ToInt32(animalenergy);
+                    string feededenergy = Convert.ToString(energyinnumbers + Elephant.Eat());
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(Elephant.Name() + animalnumber + " Energy:" + feededenergy));
+                    dtbl.Rows[i][0] = (Convert.ToString(Elephant.Name()) + animalnumber);
+                    dtbl.Rows[i][1] = feededenergy;
+                    i++;
+                }
+                else
+                { i++; }
+            }
+
+
+        }
+
+        private void FeedMonkeys_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < listBox.Items.Count)
+            {
+                string storage = Convert.ToString(listBox.Items[i]);
+                if (storage.Contains(Convert.ToString(Monkey.Name())))
+                {
+                    string animaltext = Convert.ToString(listBox.Items[i]);
+                    string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(Monkey.Name()) }, StringSplitOptions.None).Last();
+                    string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                    string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                    int energyinnumbers = Convert.ToInt32(animalenergy);
+                    string feededenergy = Convert.ToString(energyinnumbers + Monkey.Eat());
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(Monkey.Name() + animalnumber + " Energy:" + feededenergy));
+                    dtbl.Rows[i][0] = (Convert.ToString(Monkey.Name()) + animalnumber);
+                    dtbl.Rows[i][1] = feededenergy;
+                    i++;
+                }
+                else
+                { i++; }
+            }
+        }
+
+        private void FeedLions_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < listBox.Items.Count)
+            {
+                string storage = Convert.ToString(listBox.Items[i]);
+                if (storage.Contains(Convert.ToString(LionNonlead.Name())))
+                {
+                    string animaltext = Convert.ToString(listBox.Items[i]);
+                    string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(LionNonlead.Name()) }, StringSplitOptions.None).Last();
+                    string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                    string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                    int energyinnumbers = Convert.ToInt32(animalenergy);
+                    string feededenergy = Convert.ToString(energyinnumbers + LionNonlead.Eat());
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(LionNonlead.Name() + animalnumber + " Energy:" + feededenergy));
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(LionNonlead.Name() + animalnumber + " Energy:" + feededenergy));
+                    dtbl.Rows[i][0] = (Convert.ToString(LionNonlead.Name()) + animalnumber);
+                    dtbl.Rows[i][1] = feededenergy;
+                    i++;
+                }
+                else
+                { i++; }
+            }
+        }
+
+        private void FeedElephants_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            while (i < listBox.Items.Count)
+            {
+                string storage = Convert.ToString(listBox.Items[i]);
+                if (storage.Contains(Convert.ToString(Elephant.Name())))
+                {
+                 
+                    string animaltext = Convert.ToString(listBox.Items[i]);
+                    string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(Elephant.Name()) }, StringSplitOptions.None).Last();
+                    string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                    string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                    int energyinnumbers = Convert.ToInt32(animalenergy);
+                    string feededenergy = Convert.ToString(energyinnumbers + Elephant.Eat());
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(Elephant.Name() + animalnumber + " Energy:" + feededenergy));
+                    listBox.Items.RemoveAt(i);
+                    listBox.Items.Insert(i, Convert.ToString(Elephant.Name() + animalnumber + " Energy:" + feededenergy));
+                    dtbl.Rows[i][0] = (Convert.ToString(Elephant.Name()) + animalnumber);
+                    dtbl.Rows[i][1] = feededenergy;
+                    i++;
+                }
+                else
+                { i++; }
+            }
+        }
+
+
+
+        private void CreateMonkey_Click(object sender, EventArgs e)
+        {
+            amountofmonkeys += 1;
+            listBox.Items.Add(Convert.ToString(Monkey.Name()) + Convert.ToString(amountofmonkeys) + " Energy:" + (Convert.ToString(Monkey.Energy())));
+
+            if (createTable == true)
+            {
+
+                dtbl.Columns.Add("Animal");
+                dtbl.Columns.Add("Energy");
+                dataGridView.DataSource = dtbl;
+                dtbl.Rows.Add(Convert.ToString(Monkey.Name()) + Convert.ToString(amountofmonkeys), Monkey.Energy());
+                createTable = false;
+            }
+            else
+            {
+                dataGridView.DataSource = dtbl;
+                dtbl.Rows.Add(Convert.ToString(Monkey.Name()) + Convert.ToString(amountofmonkeys), Monkey.Energy());
+            }
+
+        }
+
+        private void CreateLion_Click(object sender, EventArgs e)
+        {
+            amountoflions += 1;
+            listBox.Items.Add(Convert.ToString(LionNonlead.Name()) + Convert.ToString(amountoflions) + " Energy:" + (Convert.ToString(LionNonlead.Energy())));
+
+
+            if (createTable == true)
+            {
+
+                dtbl.Columns.Add("Animal");
+                dtbl.Columns.Add("Energy");
+                dataGridView.DataSource = dtbl;
+                dtbl.Rows.Add(Convert.ToString(LionNonlead.Name()) + Convert.ToString(amountoflions), LionNonlead.Energy());
+                createTable = false;
+            }
+            else
+            {
+                dataGridView.DataSource = dtbl;
+                dtbl.Rows.Add(Convert.ToString(LionNonlead.Name()) + Convert.ToString(amountoflions), LionNonlead.Energy());
+            }
+        }
+
+        private void CreateElephant_Click(object sender, EventArgs e)
+        {
+            amountofelephants += 1;
+            listBox.Items.Add(Convert.ToString(Elephant.Name()) + Convert.ToString(amountofelephants) + " Energy:" + (Convert.ToString(Elephant.Energy())));
+
+
+            if (createTable == true)
+            {
+
+                dtbl.Columns.Add("Animal");
+                dtbl.Columns.Add("Energy");
+                dataGridView.DataSource = dtbl;
+                dtbl.Rows.Add(Convert.ToString(Elephant.Name()) + Convert.ToString(amountofelephants), Elephant.Energy());
+                createTable = false;
+            }
+            else
+            {
+                dataGridView.DataSource = dtbl;
+                dtbl.Rows.Add(Convert.ToString(Elephant.Name()) + Convert.ToString(amountofelephants), Elephant.Energy());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Timer timer = new Timer();
+            timer.Interval = (500);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            if (listBox.Items.Count > 0)
+            {
+                {
+                    DerivedClass x = new DerivedClass();
+                    int i = 0;
+                    while (i < listBox.Items.Count)
+                    {
+                        string storage = Convert.ToString(listBox.Items[i]);
+                        if (storage.Contains(Convert.ToString(Monkey.Name())))
+                        {
+                            string animaltext = Convert.ToString(listBox.Items[i]);
+                            string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(Monkey.Name()) }, StringSplitOptions.None).Last();
+                            string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                            string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                            int energyinnumbers = Convert.ToInt32(animalenergy);
+                            string feededenergy = Convert.ToString(energyinnumbers - (x.monkeyEnergy));
+                            if (feededenergy != "0")
+                            {
+                                listBox.Items.RemoveAt(i);
+                                listBox.Items.Insert(i, Convert.ToString(Monkey.Name() + animalnumber + " Energy:" + feededenergy));
+                                dtbl.Rows[i][0] = (Convert.ToString(Monkey.Name()) + animalnumber);
+                                dtbl.Rows[i][1] = feededenergy;
+                                i++;
+                            }
+                            else
+                            {
+                                listBox.Items.RemoveAt(i);
+                                dtbl.Rows.RemoveAt(i);
+                            }
+                        }
+                        else
+                        if (storage.Contains(Convert.ToString(LionNonlead.Name())))
+                        {
+                            string animaltext = Convert.ToString(listBox.Items[i]);
+                            string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(LionNonlead.Name()) }, StringSplitOptions.None).Last();
+                            string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                            string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                            int energyinnumbers = Convert.ToInt32(animalenergy);
+                            string feededenergy = Convert.ToString(energyinnumbers - (x.lionEnergy));
+                            if (feededenergy != "0")
+                            {
+                                listBox.Items.RemoveAt(i);
+                                listBox.Items.Insert(i, Convert.ToString(LionNonlead.Name() + animalnumber + " Energy:" + feededenergy));
+                                dtbl.Rows[i][0] = (Convert.ToString(LionNonlead.Name()) + animalnumber);
+                                dtbl.Rows[i][1] = feededenergy;
+                                i++;
+                            }
+                            else
+                            {
+                                listBox.Items.RemoveAt(i);
+                                dtbl.Rows.RemoveAt(i);
+                            }
+                        }
+                        else
+                        if (storage.Contains(Convert.ToString(Elephant.Name())))
+                        {
+                            string animaltext = Convert.ToString(listBox.Items[i]);
+                            string animalnumbercutofbefore = animaltext.Split(new[] { Convert.ToString(Elephant.Name()) }, StringSplitOptions.None).Last();
+                            string animalnumber = animalnumbercutofbefore.Split(new[] { " " }, StringSplitOptions.None).First();
+                            string animalenergy = animaltext.Split(new[] { "Energy:" }, StringSplitOptions.None).Last();
+                            int energyinnumbers = Convert.ToInt32(animalenergy);
+                            string feededenergy = Convert.ToString(energyinnumbers - (x.elephantEnergy));
+                            if (feededenergy != "0")
+                            {
+                                listBox.Items.RemoveAt(i);
+                                listBox.Items.Insert(i, Convert.ToString(Elephant.Name() + animalnumber + " Energy:" + feededenergy));
+                                dtbl.Rows[i][0] = (Convert.ToString(Elephant.Name()) + animalnumber);
+                                dtbl.Rows[i][1] = feededenergy;
+                                i++;
+                            }
+                            else
+                            {
+                                listBox.Items.RemoveAt(i);
+                                dtbl.Rows.RemoveAt(i);
+                            }
+
+                        }
+
+                    }
+                }
+
+
+
+
+            }
+        }
+    }
+}
