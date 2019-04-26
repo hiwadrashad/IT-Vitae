5521defe3048f14e4a6ed21c9f31d7c0cd82427c
hiwadrashad <41074761+hiwadrashad@users.noreply.github.com>
2019-04-26 14:52:12 +0200

    Add files via upload

---
+++ Kleinste gemene veelvoud - Hiwad Rashad/Kleinste gemene veelvoud - Hiwad Rashad/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Kleinste_gemene_veelvoud___Hiwad_Rashad
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Kleinste gemene veelvoud - Hiwad Rashad/Kleinste gemene veelvoud - Hiwad Rashad/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kleinste_gemene_veelvoud___Hiwad_Rashad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int firstnumber = Convert.ToInt32(NumberOne.Text);
+            int secondnumber = Convert.ToInt32(NumberTwo.Text);
+            int i = 0;
+            bool found = false;
+    
+
+            while (found == false)
+            {
+                i++;
+
+  
+                if (firstnumber%i == 0 && secondnumber%i == 0)
+                {
+                    Answer.Content = (firstnumber * secondnumber) / i;
+                    found = true;
+                }
+
+            }
+
+        }
+    }
+}
