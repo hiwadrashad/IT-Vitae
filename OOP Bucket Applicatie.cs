7c15a94cc287db87590bb4a83e1ec80906ecb7d0
hiwadrashad <41074761+hiwadrashad@users.noreply.github.com>
2019-12-16 15:56:13 +0100

    Add files via upload

---
+++ OOP Bucket Applicatie.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace OOP_Bucket_Applicatie
+{
+
+    public abstract class Program
+    {
+
+
+        abstract class Bucket
+        {
+            public abstract int bucket { get; set; }
+            public abstract int container { get; set; }
+            public abstract int bucketinhoud { get; set; }
+            public abstract int amountofbuckets { get; set; }
+        }
+
+
+        class Bucket2: Bucket
+        {
+            Bucket bucketobject = new Bucket();
+
+        }
+
+        abstract class RainBarrel
+        {
+
+            public int rainbarrel { get; set; }
+
+            public bool Small { get; set; }
+
+            public bool Medium { get; set; }
+
+            public bool Big { get; set; }
+
+            public int container { get; set; }
+
+            public int rainbarrelamount { get; set; }
+
+        }
+
+        abstract class OilBarrel
+        {
+            public int oilbarrel { get; set; }
+
+            public int container { get; set; }
+
+            public int oilbarrelamount { get; set; }
+
+        }
+
+        public class subclass2: SubClass
+        {
+    
+            
+            public static void method(): base()
+            {
+                
+                SubClass something = new SubClass();
+              
+            }
+
+        }
+    }
+}
+
